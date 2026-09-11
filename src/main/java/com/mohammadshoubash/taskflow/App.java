feb61afb752868a29461a2a908eec7490810cf98
mohammadshoubash <mohammadshoubash1@gmail.com>
2026-09-11 14:09:06 +0300

    feat: initialize database connection and start H2 web console server in main application entry point

--- src/main/java/com/mohammadshoubash/taskflow/App.java
+++ src/main/java/com/mohammadshoubash/taskflow/App.java
@@ -1,13 +1,16 @@
 package com.mohammadshoubash.taskflow;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-       System.out.println("Hello World!");
+import org.h2.tools.Server;
+import com.mohammadshoubash.taskflow.config.ConnectionManager;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ConnectionManager.initializeDatabase();
+        
+        Server webServer = Server.createWebServer("-webPort", "8082", "-tcpAllowOthers").start();
+        
+        System.out.println("H2 Web Console running at: http://localhost:8082");
+        
+        Thread.currentThread().join(); 
     }
 }
